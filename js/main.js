bbaa3a5ceeff7b85be9b1e86ce9f74d49d389f5c
BadrShahin <badr.shahin2014@gmail.com>
2017-11-06 23:56:27 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -24,4 +24,9 @@ angular.module("myApp", []).controller("myCtrl", function($scope){
 	{info_desc:'Languages', info_value: 'Arabic - English'},
 
 	];
+	$scope.education=[
+	{school_name:'Mostafa Kamel Preparatory school', year: '2008 - 2011', content: 'I graduated from Preparatory school with an Excellent Degree (91%).', courses: ''},
+	{school_name:'High secondary school', year: '2011 - 2013', content: 'I graduated from High secondary school with an Excellent Degree (93.2%).', courses: ''},
+	{school_name:'Faculty of Computer and Information', year: '2013 - 2017', content: 'I graduated from faculty of Computer and Information Menofia University with grade good (71.63%).', courses: 'Faculty Studied Courses Operating systems, Database Design, Software Engineering Java Programming, Computer Network, Data Structure and System Analysis.'},
+	];
 });
